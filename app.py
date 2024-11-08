b5ad1fdfc38ebad57415c473a0e0ba74b27174e3
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-08 14:54:43 -0300

    Configuracion de inicio de APP con configuracion de SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from models import db
+from routes import api
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gestion_gastos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == '__main__':
-  app.run(debug=True)
+db.init_app(app)
+app.register_blueprint(api, url_prefix='/api')
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
